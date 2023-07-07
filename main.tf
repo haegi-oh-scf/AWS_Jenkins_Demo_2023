8da18e7bd1706810d6e939b52e942ed954743d5e
Rebecca Theismann <rebecca.theismann@accenture.com>
2023-07-07 17:28:20 -0400

    re-enable vpc

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-# resource "aws_vpc" "main" {
-#   cidr_block = "172.16.0.0/16"
-#   instance_tenancy = "default"
-#   tags = {
-#     Name = "main"
-#   }
-# }
+resource "aws_vpc" "main" {
+  cidr_block = "172.16.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "main"
+  }
+}
