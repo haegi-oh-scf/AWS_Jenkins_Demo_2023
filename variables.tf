b6f3654b37de3d317ae8557186a694c39ad4a63a
Rebecca Theismann <stormysea22@gmail.com>
2023-07-07 15:34:12 -0400

    Update variables.tf
    updated keys

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "AWS-Jenkins-Demo" 
 }
 
 variable "instance_type" { 
