068358f9b223022e49866430776838742d59eab4
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-11 12:22:37 -0400

    Update main.tf
    adding AWS Lamda function

--- main.tf
+++ main.tf
@@ -57,6 +57,15 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
  policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/python/hello-python.zip"
+function_name                  = "Spacelift_Test_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
 data "archive_file" "zip_the_python_code" {
 type        = "zip"
 source_dir  = "${path.module}/python/"
