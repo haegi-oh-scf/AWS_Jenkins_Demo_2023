f989e22dde2def8dbf845eedd2d8c3223a6c02d4
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-11 12:15:29 -0400

    Update main.tf
    Adding IAM Lambda role

--- main.tf
+++ main.tf
@@ -9,3 +9,22 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_iam_role" "lambda_role" {
+name   = "Spacelift_Test_Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
