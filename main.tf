ae3a1c288377bbacbcedc0e2e966ac1042d92730
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-11 12:16:16 -0400

    Update main.tf
    Adding IAM policies

--- main.tf
+++ main.tf
@@ -28,3 +28,26 @@ assume_role_policy = <<EOF
 }
 EOF
 }
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
