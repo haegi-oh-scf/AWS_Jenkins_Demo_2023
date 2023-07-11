5d369fa835bd7258ead06820d50bef81a5aebd1d
Haegi Oh <103690264+haegi-oh-scf@users.noreply.github.com>
2023-07-11 15:05:18 -0400

    Update main.tf
    Adding more permissions to IAM Create Role

--- main.tf
+++ main.tf
@@ -10,79 +10,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-#assume role
-resource "aws_iam_role" "role" {
-      name = "test-alb-logs-to-elk"
-      path = "/"
-
-      assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-    EOF
-    }
-
-# Created Policy for IAM Role (s3 and log access)
-resource "aws_iam_policy" "policy" {
-  name = "my-test-policy"
-  description = "A test policy"
-
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-          "s3:Get*",
-          "s3:List*"
-          ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:*"
-            ],
-            "Resource": "arn:aws:logs:*:*:*"
-        }
-  ]
-}
-EOF
-}
-
-# Attached IAM Role and the new created Policy
-resource "aws_iam_role_policy_attachment" "test-attach" {
-  role       = "${aws_iam_role.role.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
-}
-
-
-
-# Created AWS Lamdba Function: Memory Size, NodeJS version, handler, endpoint, doctype and environment settings
-resource "aws_lambda_function" "elb_logs_to_elasticsearch" {
-  function_name = "mytest-alb-logs-to-elk"
-  description   = "elb-logs-to-elasticsearch"
-  memory_size = 1024
-  filename         = "terraform_function.zip"
-  runtime          = "nodejs8.10"
-  role             = "${aws_iam_role.role.arn}"
-  handler          = "index.handler"
-
-
-}
-/*
 resource "aws_iam_role" "lambda_role" {
 name   = "Spacelift_Test_Lambda_Function_Role"
 assume_role_policy = <<EOF
@@ -90,7 +17,12 @@ assume_role_policy = <<EOF
  "Version": "2012-10-17",
  "Statement": [
    {
-     "Action": "sts:AssumeRole",
+     "Action": [
+                "iam:AttachRolePolicy",
+                "iam:CreateRole",
+                "iam:CreatePolicy",
+                "iam:PutRolePolicy"
+            ],
      "Principal": {
        "Service": "lambda.amazonaws.com"
      },
@@ -145,4 +77,3 @@ source_dir  = "${path.module}/python/"
 output_path = "${path.module}/python/hello-python.zip"
 }
 
-*/
